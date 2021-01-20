70d460362345af7714926e3ac43f8f0d16a5a99d
Cikii <71007204+Cikii@users.noreply.github.com>
2021-01-20 21:05:10 +0100

    Add files via upload
    verze 1.1

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
 const dino = document.getElementById("dino");
 const kaktus = document.getElementById("kaktus");
+let sekundy = 0;
+let minuty = 0;
+let milisekundy = 0;
+let myVar;
+let stopky2 = window.setInterval(stopky, -10);
 
 function jump() {
     if (dino.classList != "jump"){
@@ -15,12 +20,30 @@ let zivot = setInterval(function(){
     let kaktusLeft = parseInt(window.getComputedStyle(kaktus).getPropertyValue("left")); 
 
     if (kaktusLeft<38 && kaktusLeft > 0 && dinoTop>= 140){
-        alert("Konec hry.")
+        alert("Konec hry. Čas je: " + minuty + ":" + sekundy + "." + milisekundy)
+        clearTimeout(myVar);
+        window.location.reload();
     }
 
 }, 10);
 
-document.addEventListener("keydown", function (event){
-    jump();
-
+document.addEventListener("keydown", function (){
+       jump();
+            
 });
+
+
+function stopky(){
+    milisekundy++;
+    if(milisekundy === 100){
+        milisekundy = 0;
+        sekundy ++;
+
+        if(sekundy === 60 ){
+            sekundy = 0;
+            minuty++;
+        }
+    }
+    
+    document.getElementById("display").innerHTML = minuty + ":" + sekundy + "." + milisekundy;
+}
